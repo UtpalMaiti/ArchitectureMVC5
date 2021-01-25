d9d6682269c8806cb655fb1c570ce30667372df7
Utpal Maiti <Utpalmaiti12@gmail.com>
2021-01-25 14:05:37 +0530

    Initial Commit

---
+++ SMS.ALL.MVC/6.0.0/aspnet-core/src/SMS.ALL.Web.Mvc/Controllers/AboutController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Abp.AspNetCore.Mvc.Authorization;
+using SMS.ALL.Controllers;
+
+namespace SMS.ALL.Web.Controllers
+{
+    [AbpMvcAuthorize]
+    public class AboutController : ALLControllerBase
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
---
+++ SMS.ALL.MVC/6.0.0/aspnet-core/src/SMS.ALL.Web.Mvc/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Abp.AspNetCore.Mvc.Authorization;
+using SMS.ALL.Controllers;
+
+namespace SMS.ALL.Web.Controllers
+{
+    [AbpMvcAuthorize]
+    public class HomeController : ALLControllerBase
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
